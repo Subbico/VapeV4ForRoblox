681b819423b3e496b9b8ff86f07d6f22816d3452
wrealaero <clanwarobi@gmail.com>
2026-01-19 11:35:36 -0600

    Initialize AccountSystem with sample accounts

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -1,1 +1,16 @@
-eeeee
+local AccountSystem = {}
+
+AccountSystem.Accounts = {
+    {
+        Username = "aero",
+        Password = "aero123",
+        IsActive = true
+    },
+    {
+        Username = "yin",
+        Password = "yin123",
+        IsActive = true
+    },
+}
+
+return AccountSystem
